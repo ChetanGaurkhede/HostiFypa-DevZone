3b9af951e19686f2008a13be612622624c09b9b3
Chetan Gaurkhede <gaurkhede732@gmail.com>
2024-09-16 21:48:38 +0530

    footer setting added

--- src/assets/componants/WrapSlider/WrapSlidder.jsx
+++ src/assets/componants/WrapSlider/WrapSlidder.jsx
@@ -9,8 +9,34 @@ function WrapSlidder() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <>
